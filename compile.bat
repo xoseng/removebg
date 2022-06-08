1a3312fe65a5e8545a5493a523050812615b99b8
Xosé <46671856+xoseng@users.noreply.github.com>
2022-06-08 09:22:50 +0200

    Add files via upload

---
+++ compile.bat
@@ -0,0 +1,3 @@
+cd C:\Users\%USERNAME%\Desktop\removebg
+C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python38\python.exe C:\Users\%USERNAME%\Desktop\removebg\setup.py build
+pause
---
+++ setup.py
@@ -0,0 +1,17 @@
+# coding=utf8
+#launch from cmd: python setup.py build
+     
+from cx_Freeze import setup, Executable
+
+target = Executable(
+    script="removebg.py",
+    base="logo.ico"
+    )
+
+setup(
+    name="RemoveBG",
+    version="0.1",
+    description="RemoveBG - Remove image background software",
+    author="Xosé Brais Noya García",
+    executables=[target]
+    )
